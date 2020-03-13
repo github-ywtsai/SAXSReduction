ee0ed6ff9954590cf67b5bdabf61e800498453c7
skull <skull@DESKTOP-96P2JAM>
2020-03-13 17:41:11 +0800

    When data only contain 1 data sheep, disable the data selection spinner.

--- @GeneralFunc/UpdateDataSheetSelectionItems.m
+++ @GeneralFunc/UpdateDataSheetSelectionItems.m
@@ -1,4 +1,10 @@
 function UpdateDataSheetSelectionItems(app, event)
 
 app.CurrentDataSheetSpinner.Value = 1;
-app.CurrentDataSheetSpinner.Limits = double([1 app.CurrentData.MasterInfo.DataSheetNum]);
+
+if app.CurrentData.MasterInfo.DataSheetNum == 1
+    app.CurrentDataSheetSpinner.Enable = 'off';
+else
+    app.CurrentDataSheetSpinner.Enable = 'on';
+    app.CurrentDataSheetSpinner.Limits = double([1 app.CurrentData.MasterInfo.DataSheetNum]);
+end
