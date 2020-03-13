6922bb7a564f89a298319cc29903c76417e2f171
skull <skull@DESKTOP-96P2JAM>
2020-03-13 10:34:29 +0800

    Add log/linear intensity for 2D image

--- @GeneralFunc/PlotCurrentImage.m
+++ @GeneralFunc/PlotCurrentImage.m
@@ -2,3 +2,10 @@ function PlotCurrentImage(app,event)
 
 app.PlotHandles.Image = imagesc(app.ImageUIAxes,app.CurrentData.ImageForDrawing);
 axis(app.ImageUIAxes,'image')
+colorbar(app.ImageUIAxes)
+
+if app.ImageIntensityScaleButtonGroup.SelectedObject == app.ImageIntensityLogButton
+    app.ImageUIAxes.ColorScale = 'log';
+else
+    app.ImageUIAxes.ColorScale = 'linear';
+end
