a00b7a1b5b12f01a42cfa07ee7083df619871814
skull <skull@172.18.39.182>
2020-03-18 18:02:15 +0800

    DataProcess can handle app.CurrentData.RawData includeing multidata, prepared for average data process.

--- @GeneralFunc/DataProcess.m
+++ @GeneralFunc/DataProcess.m
@@ -1,5 +1,7 @@
 function DataProcess(app,event)
 
+NumData = size(app.CurrentData.RawData,3);
+
 if isempty(app.BGInfo.Background)
     NormRawData = app.CurrentData.RawData/app.CurrentData.MasterInfo.CountTime;
 else
@@ -8,19 +10,23 @@ end
 
 % 2D image part
 if isempty(app.CurrentData.MaskInfo.EffectiveMask)
-    app.CurrentData.ImageForDrawing = NormRawData;
+    app.CurrentData.ImageForDrawing = sum(NormRawData,3)/NumData;
 else
-    app.CurrentData.ImageForDrawing = NormRawData.* ~app.CurrentData.MaskInfo.EffectiveMask;
+    app.CurrentData.ImageForDrawing = sum(NormRawData.* ~app.CurrentData.MaskInfo.EffectiveMask,3)/NumData;
 end
 
 % 1D profile part
-RawDataVector = NormRawData(:);
 GuidingIdxVector = app.CurrentData.ImageProfileConvertor.qSpace.GuidingIdxVector;
 XAxis = app.CurrentData.ImageProfileConvertor.qSpace.Axis;
 NumPixelInqSection = app.CurrentData.ImageProfileConvertor.qSpace.NumPixelInqSection;
-
 IgnoreIdx = app.CurrentData.ImageProfileConvertor.qSpace.IgnoreIdx;
-RawDataVector(IgnoreIdx) = [];
+IntensityTemp = zeros(NumData,length(XAxis));
+for DataSN = 1:NumData
+    NormRawDataTemp = NormRawData(:,:,DataSN);
+    RawDataVector = NormRawDataTemp(:);
+    RawDataVector(IgnoreIdx) = [];
 
-Intensity = transpose(accumarray(GuidingIdxVector,RawDataVector))./NumPixelInqSection;
+    IntensityTemp(DataSN,:) = transpose(accumarray(GuidingIdxVector,RawDataVector))./NumPixelInqSection;
+end
+Intensity = sum(IntensityTemp,1)/NumData;
 app.CurrentData.ProfileForDrawing = [XAxis;Intensity];
