6c14e54a675f156070e22f73cc3345d63d916301
skull <skull@172.18.39.182>
2020-04-16 17:08:53 +0800

    Add OS support with PC and UNIX

--- @GeneralFunc/EnvSetting.m
+++ @GeneralFunc/EnvSetting.m
@@ -1,22 +1,36 @@
 function EnvSettingCheck = EnvSetting()
-    DLLPATH = fullfile(pwd,'./@EigerDataFunc');
-    cmd = sprintf('setx HDF5_PLUGIN_PATH "%s', DLLPATH);
-    HDF5PLUGINPATH = getenv('HDF5_PLUGIN_PATH');
-    
-    if ~isempty(HDF5PLUGINPATH)
-        HDF5PLUGINPATH = fullfile(HDF5PLUGINPATH);
-        if ~strcmpi(HDF5PLUGINPATH,DLLPATH)
+
+if ispc
+    OS = 'PC';
+elseif isunix
+    OS = 'UNIX';
+elseif ismac
+    OS = 'MAC';
+end
+
+switch OS
+    case 'PC'
+        DLLPATH = fullfile(pwd,'./@EigerDataFunc');
+        cmd = sprintf('setx HDF5_PLUGIN_PATH "%s', DLLPATH);
+        HDF5PLUGINPATH = getenv('HDF5_PLUGIN_PATH');
+        
+        if ~isempty(HDF5PLUGINPATH)
+            HDF5PLUGINPATH = fullfile(HDF5PLUGINPATH);
+            if ~strcmpi(HDF5PLUGINPATH,DLLPATH)
+                system(cmd);
+                disp('Environment variables are arranged.')
+                disp('Please restart Matlab.')
+                EnvSettingCheck = false;
+            else
+                EnvSettingCheck = true;
+            end
+        else
             system(cmd);
             disp('Environment variables are arranged.')
             disp('Please restart Matlab.')
             EnvSettingCheck = false;
-        else
-            EnvSettingCheck = true;
-        end 
-    else
-        system(cmd);
-        disp('Environment variables are arranged.')
-        disp('Please restart Matlab.')
-        EnvSettingCheck = false;
-    end
+        end
+    case 'UNIX'
+        DLLPATH = fullfile(pwd,'./@EigerDataFunc');
+        setenv('HDF5_PLUGIN_PATH',DLLPATH);
 end
