762345acccf2fff6d5da1cd6abef89156e7ccc3a
skull <skull@YWTSAI-XPS7390>
2020-04-21 11:53:37 +0800

    Change data format from double to single when data importing.

--- @UserCallback/DataSheetSelect.m
+++ @UserCallback/DataSheetSelect.m
@@ -1,4 +1,5 @@
 function DataSheetSelect(app,event)
+
 if event.Source == app.CurrentDataSheetSpinner
     SourceItem = 'Spinner';
 elseif event.Source == app.EigerMasterMenu
@@ -19,6 +20,7 @@ end
 app.CurrentData.RawData = single(EigerDataFunc.ReadEigerHDF5Data(app.CurrentData.MasterInfo,app.CurrentData.RequestSN,[],[]));
 [~,Title,~] = fileparts(app.CurrentData.MasterInfo.MasterFP); Title = strrep(Title,'_master','');
 app.CurrentData.Title = sprintf('%s#%d',Title,app.CurrentData.RequestSN);
+
 
 GeneralFunc.DataProcess(app,event);
 GeneralFunc.PlotCurrentProfile(app,event);
