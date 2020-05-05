874476ed22291a8e20b4af3123269d9303407585
skull <skull@172.18.39.182>
2020-05-05 15:46:50 +0800

    Add skip data sheet function for average data function

--- @UserCallback/AverageData.m
+++ @UserCallback/AverageData.m
@@ -1,9 +1,7 @@
 function AverageData(app,event)
 GeneralFunc.MessageControl(app,event,'Start to average data...','add');
-StartSN = app.AvgDataStartSNEditField.Value;
-Increment = app.AvgDataIncrementEditField.Value;
-EndSN = app.AvgDataEndSNEditField.Value;
-RequestSNList = StartSN:Increment:EndSN;
+
+RequestSNList = GenRequestSNList(app);
 
 app.CurrentData.RequestSN = RequestSNList;
 
@@ -20,10 +18,24 @@ end
 
 app.CurrentData.RawData = DataContainer;
 [~,Title,~] = fileparts(app.CurrentData.MasterInfo.MasterFP); Title = strrep(Title,'_master','');
-app.CurrentData.Title = sprintf('%s#%d:%d:%d',Title,StartSN,Increment,EndSN);
+app.CurrentData.Title = sprintf('%s#%d:%d:%d',Title,app.AvgDataStartSNEditField.Value,app.AvgDataIncrementEditField.Value,app.AvgDataEndSNEditField.Value);
 
 GeneralFunc.MessageControl(app,event,sprintf('Processing %d data ...',NumRequest),'replace');
 GeneralFunc.DataProcess(app,event);
 GeneralFunc.MessageControl(app,event,sprintf('Processing %d data ... Done',NumRequest),'replace');
 GeneralFunc.PlotCurrentImage(app,event);
 GeneralFunc.PlotCurrentProfile(app,event);
+
+function RequestSNList = GenRequestSNList(app)
+StartSN = app.AvgDataStartSNEditField.Value;
+Increment = app.AvgDataIncrementEditField.Value;
+EndSN = app.AvgDataEndSNEditField.Value;
+RequestSNList = StartSN:Increment:EndSN;
+
+SkipSNString = app.AvgDataSkipSNEditField.Value;
+SkipSNString = strrep(SkipSNString,';',',');
+SkipSN = str2num(SkipSNString);
+NSkip = length(SkipSN);
+for idx = 1:NSkip
+    RequestSNList(RequestSNList == SkipSN(idx)) = [];
+end
