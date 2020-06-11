64b7e82bd0c41148ae772c83ea8b22a96f9ad925
skull <skull@172.18.39.182>
2020-06-11 17:48:46 +0800

    change busy control catalogy

--- @GeneralFunc/BusyControl.m
+++ @GeneralFunc/BusyControl.m
@@ -1,123 +1,112 @@
 function BusyControl(app,event,Status)
 
+% Menu
+    % menu
+MenuPart = [
+        app.ImportMenu
+        app.EigerMasterMenu
+        app.ExportMenu
+        app.CurrentallinfotomatfileMenu
+        ];
+% Buttons
+ButtonPart = [
+    % Mask control
+        app.AddCSVMaskButton
+        app.RemoveCSVMaskButton
+        app.ActiveCSVMaskButton
+        app.InvertCSVMaskButton
+    % Background control
+        app.BGSetButton
+        app.BGDrawButton
+        app.BGRemoveButton
+        app.SaveCurrentDataAsBackgroundButton
+    % Data storage
+        app.StoreCurrentDataButton
+        app.RemoveStoredDataButton
+        app.PlotSelectStoredDataButton
+        app.LoadDataButton
+    % Mask generator
+        app.MaskGenRefreshButton
+        app.ExportMaskButton
+    % Average data
+        app.AvgDataButton
+    % Effective mask control
+        app.SaveMaskPreviewButton
+        app.RemoveMaskPreviewButton
+];
+% RadioButton
+RadioButtonPart = [
+    % Experimental conditions
+        app.DefaultButton
+        app.UserDefineButton
+];
+% Spinner
+SpinnerPart = [
+    % Data select
+        app.CurrentDataSheetSpinner
+];
+% Table
+TablePart = [
+    % Mask control
+        app.CSVMaskUITable
+    % Data storage
+        app.DataStorageUITable
+    % Mask generator
+        app.RadiusMaskGenUITable
+    % Experimental condtions
+        app.ExpCondUITable
+];
+% Dropdown
+DropDownPart = [
+    % Mask control
+        app.CSVMaskIDDropDown
+    % Background control
+        app.BackgroundIDDropDown
+    % Data storage
+        app.StoredDataIDDropDown
+    % Effective mask selection
+        app.EffectiveMaskSelectionDropDown
+    % Effective mask control
+        app.EffectiveMaskIDDropDown
+];
+% NumEditField
+NumEditFieldPart = [
+    % Background control
+        app.SampleTransEditField
+        app.BufferTransEditField
+    % Average data
+        app.AvgDataStartSNEditField
+        app.AvgDataIncrementEditField
+        app.AvgDataEndSNEditField
+    % Data storage offset/magnitude
+        app.StoredDataOffsetEditField
+        app.StoredDataMagnificationEditField
+];
+% EditField
+EditFieldPart = [
+    % Average data
+        app.AvgDataSkipSNEditField
+    % Effective mask control
+        app.EffectiveMaskNameEditField
+];
+
 if Status % Busy
-    % Menu
-    app.ImportMenu.Enable = 'Off';
-    app.EigerMasterMenu.Enable = 'Off';
-    app.ExportMenu.Enable = 'Off';
-    app.CurrentallinfotomatfileMenu.Enable = 'Off';
-    drawnow
-    
-    % Data select
-    app.CurrentDataSheetSpinner.Enable = 'Off';
-    drawnow
-    
-    % Mask control
-    app.AddCSVMaskButton.Enable = 'Off';
-    app.RemoveCSVMaskButton.Enable = 'Off';
-    app.ActiveCSVMaskButton.Enable = 'Off';
-    app.InvertCSVMaskButton.Enable = 'Off';
-    app.CSVMaskUITable.Enable = 'Off';
-    app.CSVMaskIDDropDown.Enable = 'Off';
-    drawnow
-    
-    % Background control
-    app.BGSetButton.Enable = 'Off';
-    app.BGDrawButton.Enable = 'Off';
-    app.BGRemoveButton.Enable = 'Off';
-    app.SaveCurrentDataAsBackgroundButton.Enable = 'Off';
-    app.BackgroundIDDropDown.Enable = 'Off';
-    app.SampleTransEditField.Enable = 'Off';
-    app.BufferTransEditField.Enable = 'Off';
-    drawnow
-    
-    % Data storage
-    app.DataStorageUITable.Enable  = 'Off';
-    app.StoredDataIDDropDown.Enable = 'Off';
-    app.StoreCurrentDataButton.Enable = 'Off';
-    app.RemoveStoredDataButton.Enable = 'Off';
-    app.PlotSelectStoredDataButton.Enable = 'Off';
-    app.LoadDataButton.Enable = 'Off';
-    drawnow
-    
-    % Mask generator
-    app.RadiusMaskGenUITable.Enable = 'Off';
-    app.MaskGenRefreshButton.Enable = 'Off';
-    app.ExportMaskButton.Enable = 'Off';
-    drawnow
-    
-    % Experimental condtions
-    app.ExpCondUITable.Enable = 'Off';
-    app.DefaultButton.Enable = 'Off';
-    app.UserDefineButton.Enable = 'Off';
-    drawnow
-    
-    % Average data
-    app.AvgDataButton.Enable = 'Off';
-    app.AvgDataStartSNEditField.Enable = 'Off';
-    app.AvgDataIncrementEditField.Enable = 'Off';
-    app.AvgDataEndSNEditField.Enable = 'Off';
-    app.AvgDataSkipSNEditField.Enable = 'Off';
-    drawnow
-    
+[MenuPart(:).Enable] = deal('off');
+[ButtonPart(:).Enable] = deal('off');
+[RadioButtonPart(:).Enable] = deal('off');
+[SpinnerPart(:).Enable] = deal('off');
+[TablePart(:).Enable] = deal('off');
+[DropDownPart(:).Enable] = deal('off');
+[NumEditFieldPart(:).Enable] = deal('off');
+[EditFieldPart(:).Enable] = deal('off');
 else % Release
-    % Menu
-    app.ImportMenu.Enable = 'On';
-    app.EigerMasterMenu.Enable = 'On';
-    app.ExportMenu.Enable = 'On';
-    app.CurrentallinfotomatfileMenu.Enable = 'On';
-    
-    drawnow
-    
-    % Data select
-    app.CurrentDataSheetSpinner.Enable = 'On';
-    drawnow
-    
-    % Mask control
-    app.AddCSVMaskButton.Enable = 'On';
-    app.RemoveCSVMaskButton.Enable = 'On';
-    app.ActiveCSVMaskButton.Enable = 'On';
-    app.InvertCSVMaskButton.Enable = 'On';
-    app.CSVMaskUITable.Enable = 'On';
-    app.CSVMaskIDDropDown.Enable = 'On';
-    drawnow
-    
-    % Background control
-    app.BGSetButton.Enable = 'On';
-    app.BGDrawButton.Enable = 'On';
-    app.BGRemoveButton.Enable = 'On';
-    app.SaveCurrentDataAsBackgroundButton.Enable = 'On';
-    app.BackgroundIDDropDown.Enable = 'On';
-    app.SampleTransEditField.Enable = 'On';
-    app.BufferTransEditField.Enable = 'On';
-    drawnow
-    
-    % Data storage
-    app.DataStorageUITable.Enable  = 'On';
-    app.StoredDataIDDropDown.Enable = 'On';
-    app.StoreCurrentDataButton.Enable = 'On';
-    app.RemoveStoredDataButton.Enable = 'On';
-    app.PlotSelectStoredDataButton.Enable = 'On';
-    app.LoadDataButton.Enable = 'On';
-    drawnow
-    
-    % Mask generator
-    app.RadiusMaskGenUITable.Enable = 'On';
-    app.MaskGenRefreshButton.Enable = 'On';
-    app.ExportMaskButton.Enable = 'On';
-    drawnow
-    
-    % Experimental condtions
-    app.ExpCondUITable.Enable = 'On';
-    app.DefaultButton.Enable = 'On';
-    app.UserDefineButton.Enable = 'On';
-    drawnow
-    
-    % Average data
-    app.AvgDataButton.Enable = 'On';
-    app.AvgDataStartSNEditField.Enable = 'On';
-    app.AvgDataIncrementEditField.Enable = 'On';
-    app.AvgDataEndSNEditField.Enable = 'On';
-    app.AvgDataSkipSNEditField.Enable = 'On';
-    drawnow
+[MenuPart(:).Enable] = deal('on');
+[ButtonPart(:).Enable] = deal('on');
+[RadioButtonPart(:).Enable] = deal('on');
+[SpinnerPart(:).Enable] = deal('on');
+[TablePart(:).Enable] = deal('on');
+[DropDownPart(:).Enable] = deal('on');
+[NumEditFieldPart(:).Enable] = deal('on');
+[EditFieldPart(:).Enable] = deal('on');
 end
+drawnow nocallbacks
