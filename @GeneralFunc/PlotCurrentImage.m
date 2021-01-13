fea51af4ff71712d362678b8532944bfe51c14e2
skull <skull@172.18.36.32>
2021-01-13 17:00:45 +0800

    Add x and y axis using qz and qy

--- @GeneralFunc/PlotCurrentImage.m
+++ @GeneralFunc/PlotCurrentImage.m
@@ -3,10 +3,18 @@ function PlotCurrentImage(app,event)
 ImageForDrawimg = app.CurrentData.ImageForDrawing;
 [RowSize,ColSize] = size(ImageForDrawimg);
 
-app.PlotHandles.Image = imagesc(app.ImageUIAxes,ImageForDrawimg);
+% prepare xaxis and yaxis
+xAxis = app.CurrentData.ImageProfileConvertor.ImgqyAxis;
+yAxis = app.CurrentData.ImageProfileConvertor.ImgqzAxis;
+xAxisLabel = sprintf('q_y(1/%c)',char(197));
+yAxisLabel = sprintf('q_z(1/%c)',char(197));
+
+app.PlotHandles.Image = imagesc(app.ImageUIAxes,xAxis,yAxis,ImageForDrawimg);
 axtoolbar(app.ImageUIAxes,{'datacursor','zoomin','zoomout','restoreview'});
 app.ImageUIAxes.DataAspectRatio = [1 1 1];
 app.ImageUIAxes.PlotBoxAspectRatio = [ColSize RowSize 1];
+app.ImageUIAxes.XLabel.String = xAxisLabel;
+app.ImageUIAxes.YLabel.String = yAxisLabel;
 % DataAspectRatio & PlotBoxAspectRatio configuration affect the same with
 % axis image but faster.
 % axis(app.ImageUIAxes,'image')
