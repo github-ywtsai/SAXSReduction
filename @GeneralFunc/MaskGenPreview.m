f2a71a299e3b93d2fbe9bcbd368992e9ea436887
skull <skull@172.18.36.32>
2021-01-18 09:31:17 +0800

    Copy image properties from 2D plot to mask preview

--- @GeneralFunc/MaskGenPreview.m
+++ @GeneralFunc/MaskGenPreview.m
@@ -4,7 +4,24 @@ MaskPreview = app.MaskGen.MaskPreview;
 AlphaDataForMaskPreview = ones(size(MaskPreview)) - ~MaskPreview*0.7;
 app.PlotHandles.MaskPreview = imagesc(app.MaskPreviewUIAxes,app.CurrentData.ImageForDrawing,'AlphaData',AlphaDataForMaskPreview);
 
-axis(app.MaskPreviewUIAxes,'image');
+% copy imgae configurations from app.ImageUIAxes and app.PlotHandles.Image
+% to app.MaskPreviewUIAxes and app.PlotHandles.MaskPreview
+[RowSize,ColSize] = size(app.CurrentData.ImageForDrawing);
+app.MaskPreviewUIAxes.YAxis.Direction = app.ImageUIAxes.YAxis.Direction;
+
+app.PlotHandles.MaskPreview.XData = app.PlotHandles.Image.XData; 
+app.PlotHandles.MaskPreview.YData = app.PlotHandles.Image.XData;
+
+app.MaskPreviewUIAxes.XLabel.Interpreter = app.ImageUIAxes.XLabel.Interpreter;
+app.MaskPreviewUIAxes.YLabel.Interpreter = app.ImageUIAxes.YLabel.Interpreter;
+app.MaskPreviewUIAxes.XLabel.String = app.ImageUIAxes.XLabel.String;
+app.MaskPreviewUIAxes.YLabel.String = app.ImageUIAxes.YLabel.String;
+
+app.MaskPreviewUIAxes.DataAspectRatio = [1 1 1];
+app.MaskPreviewUIAxes.PlotBoxAspectRatio = [ColSize RowSize 1];
+
+app.MaskPreviewUIAxes.XLim = [-inf inf];
+app.MaskPreviewUIAxes.YLim = [-inf inf];
 
 
 % Colormap control
@@ -15,3 +32,8 @@ app.MaskPreviewUIAxes.ColorScale = app.ImageUIAxes.ColorScale;
 
 % IntensityRangeControl
 app.MaskPreviewUIAxes.CLim = app.ImageUIAxes.CLim;
+
+% copy title
+app.MaskPreviewUIAxes.Title.String = app.ImageUIAxes.Title.String;
+app.MaskPreviewUIAxes.Title.Interpreter = app.ImageUIAxes.Title.Interpreter;
+
